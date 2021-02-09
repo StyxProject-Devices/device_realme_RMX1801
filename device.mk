69d54e4133ec904e429acd0e04c443f7fa259136
ad1tyas1ngh <adityasingh1768@gmail.com>
2021-02-09 17:18:01 +0530

    RMX1801: Styx Bringup

--- device.mk
+++ device.mk
@@ -32,7 +32,9 @@ PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
 # Soong namespaces
-PRODUCT_SOONG_NAMESPACES += device/realme/RMX1801
+PRODUCT_SOONG_NAMESPACES += \
+    device/realme/RMX1801 \
+    packages/apps/Bluetooth
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -154,7 +156,7 @@ PRODUCT_PACKAGES += \
     libbluetooth_audio_session \
     libbthost_if
 
-# Bluetooth
+# Bluetooth 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
